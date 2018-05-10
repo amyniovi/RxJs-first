7e5ae5d6ff148752da86b09576830b0658860d1f
Amy Niovi <amyniovi@gmail.com>
2018-05-10 11:40:46 +0100

    Observable event stream

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+console.log('init');
+
+const btn =  $('#btn');
+
+const btnStream$ = Rx.Observable.fromEvent(btn,'click');
+
+btnStream$.subscribe(function(e){
+console.log('e');
+},function(err){
+	console.log(err);
+},function(){
+	console.log('completed!')
+});
